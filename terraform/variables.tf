23cb53e19fd3c7d461e875b80e9fb1eb5d97147a
ShreyasRavath <34619797+ShreyasRavath@users.noreply.github.com>
2025-07-19 17:16:37 +0530

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,5 +3,5 @@ variable "primary_region" { default = "us-east-1" }
 variable "standby_region" { default = "us-east-2" }
 variable "account_id" {default = "381751878913"}
 variable "alert_email" {default = "2007shreyas@gmail.com"}
-variable "primary_cluster_name" {default = "eks-cluster-primary"}
-variable "standby_cluster_name" {default = "eks-cluster-standby"}
+variable "primary_cluster_name" {default = "primary-eks"}
+variable "standby_cluster_name" {default = "standby-eks"}
