4a8c4b626612e7be9f1a40ee455f88b605082889
ShreyasRavath <34619797+ShreyasRavath@users.noreply.github.com>
2025-07-19 13:43:25 +0530

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,5 @@
 
 variable "primary_region" { default = "us-east-1" }
 variable "standby_region" { default = "us-east-2" }
-variable "account_id" {
-  description = "AWS account ID for ECR replication"
-  type        = string
-}
+variable "account_id" {type        = string}
 
