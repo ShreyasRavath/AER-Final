190ae954204733027d398982089ee9a0008ec933
ShreyasRavath <34619797+ShreyasRavath@users.noreply.github.com>
2025-07-19 14:49:30 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,8 @@
 
 variable "primary_region" { default = "us-east-1" }
 variable "standby_region" { default = "us-east-2" }
-variable "account_id" {type        = string}
-variable "alert_email" {
-  type        = string
-  description = "Email to notify via SNS"
-}
+variable "account_id" {default = "381751878913"}
+variable "alert_email" {default = "2007shreyas@gmail.com"}
 
 
 
